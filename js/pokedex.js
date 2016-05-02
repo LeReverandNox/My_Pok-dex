d76fcee0e982f7cfacd3461645c7d117be6b0fef
laidet_r <rodolphe.laidet@epitech.eu>
2016-05-02 16:36:00 +0200

    Rajout du meme test à leftpad qu'a capitalize, soucis d'undefined

--- js/pokedex.js
+++ js/pokedex.js
@@ -171,6 +171,9 @@
 
     app.filter('leftpad', function () {
         return function (number, length) {
+            if (!number) {
+                return number;
+            }
 
             number = '' + number;
             while (number.length < length) {
